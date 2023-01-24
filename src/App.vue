21e15b9e55d76895e7a7ca4e786a851d5a060a7a
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-01-24 17:37:53 +0100

    add pokèmon from api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script>
-export default {}
+import AppMain from './components/AppMain.vue';
+export default {
+
+  components: { AppMain }
+};
 </script>
 
 <template>
-
+  <div class="container my-5 box text-center p-3">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="">
+    <header class="text-center p-5">
+    </header>
+    <AppMain />
+  </div>
 
 </template>
 
-<style>
-
+<style lang="scss">
+.box {
+  height: 1000px;
+  width: 100%;
+  background-color: #dedede;
+}
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,36 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            pokemons: []
+        }
+    },
+    mounted() {
+        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=8').then(res => { this.pokemons = res.data.docs })
+    }
+}     
+</script>
+
+<template>
+    <div class="row">
+        <!-- <h1 v-if="">Loading...</h1> -->
+        <div v-for="pokemon in pokemons" :key="pokemon.name" class="col-2 text-center p-4">
+            <img class="img-fluid" :src="pokemon.imageUrl" :alt="pokemon.name">
+            <h5>{{ pokemon.name }}</h5>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.col-2 {
+    width: calc(100% / 5)
+}
+
+img {
+    width: 230px;
+    height: 230px;
+    object-fit: fill;
+}
+</style>
